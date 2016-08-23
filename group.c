b591a4b083631d913bc6a4ef92e246fc02c40824
Keaton Wallace <keaton.wallace@gmail.com>
2016-08-22 18:00:33 -0700

    Solved for two lists. Some code cleanup. Started adding comments. Still to do, n list solution. I think I will just call my two list solution recursively.

--- group.c
+++ group.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-//If I get around to changing it the value field could have a better name
 struct node {
   int x;
   struct node *next;
 };
 
-void pushNode(struct node *head, int val);
-void generateList(struct node *head);
+struct node *pushNode(struct node *head, int val);
+struct node *generateList();
 struct node *sortList(struct node *lists[], int len);
 void displayList(struct node *head);
 struct node *makeHead();
 
-//I am traversing the whole list every time which is inefficient
-void pushNode(struct node *head, int val){
+//ARGS: List and Value
+//RETURNS: Node added to list
+//ACTION: Adds a node to the end of a list
+struct node *pushNode(struct node *head, int val){
   struct node *traverse = head;
   while (traverse->next!=NULL){
     traverse=traverse->next;
@@ -26,72 +27,62 @@ void pushNode(struct node *head, int val){
   newNode->x = val;
   newNode->next=NULL;
   traverse->next=newNode;
+  return newNode;
 }
 
-//Should generate a sorted list of integers
-void generateList(struct node *head){
+//ARGS: List
+
+struct node *generateList(){
   int j;
   int r;
-  r=rand()%10;//adding some randomness to the lists
+  struct node *head;
+  struct node *last;
+  head=makeHead();
+  last=head;
   for(j=0; j<100; j=j+r){
-    pushNode(head, j);
+    last=pushNode(last, j);
     r=random()%10;
   }
-  return;
+  return head;
 }
 
 //args an array of head nodes.
 //Pass by value if I care about preserving original lists.
 struct node *sortList(struct node *lists[], int len){
   struct node *head;
+  struct node *last;
   int i;
   int index=0;
-
-  for(i=0; i<len; i++){
-    lists[i]=lists[i]->next;//advance past head nodes
-  }
-  int min = lists[0]->x;//ignore the head node
+  int min=lists[0]->x;
   head=makeHead();
-  printf("about to sort\n");
-  while(lists[0]!=NULL){
-    printf("min is %i \n", min);
+  last=head;
+  while(lists[0]!=NULL&&lists[1]!=NULL){
     for(i=0; i<len; i++){
-      printf("lists[%i]->x is %i ", i, lists[i]->x);
       if(lists[i]->x < min){
 	index=i;
 	min=lists[i]->x;
       }
     }
-    printf("pushing node %i\n", min);
-    pushNode(head, min);
+    last=pushNode(last, min);
     lists[index]=lists[index]->next;
-    printf("where is the beef\n");
     if(lists[index]!=NULL){
       min=lists[index]->x;
     }
-    }
-    
-  if(lists[0]==NULL){
-    while(lists[1]!=NULL){
-      pushNode(head, lists[1]->x);
-      lists[1]=lists[1]->next;
-    }
   }
-  else{
-    while(lists[0]!=NULL){
-      pushNode(head, lists[0]->x);
-      lists[0]=lists[0]->next;
-    }
+  if(lists[0]==NULL){
+    last->next=lists[1];
+  }
+  if(lists[1]==NULL){
+    last->next=lists[0];
   }
   return head;
 }
 
 void displayList(struct node *head){
-  if(head->next==NULL){
+  if(head==NULL){
     printf("Empty list\n");
     return;
   }
-  head=head->next;
   while(head->next!=NULL){
     printf("%i\n",head->x);
     head=head->next;
@@ -117,8 +108,7 @@ int main(){
   scanf("%i", &j);
   struct node *heads[j];
   for(k=0; k<j; k++){
-    heads[k]=makeHead();
-    generateList(heads[k]);
+    heads[k]=generateList();
   }
   printf("\n");
   displayList(heads[0]);
@@ -127,7 +117,4 @@ int main(){
   return 0;
 }
 
-
-//TODO: Get rid of head nodes
-//TODO: Solve for 2 lists
 //TODO: Solve for n lists
